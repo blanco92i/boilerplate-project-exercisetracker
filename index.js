6f7e5d962d127e593e5d72aa184c8d7cb404f14f
blanco92i <blancotradizi@gmail.com>
2024-11-14 16:16:59 +0000

    U

--- index.js
+++ index.js
@@ -22,11 +22,16 @@ dbConnect();
 
 //user model
 const userSchema = new mongoose.Schema({
-  username:{
-    type:String,
-    require:true
-  }
-})
+  username: {
+    type: String,
+    required: true
+  },
+  log: [{
+    description: String,
+    duration: Number,
+    date: Date
+  }]
+});
 
 let User = mongoose.model('User',userSchema)
 
@@ -67,6 +72,76 @@ app.post('/api/users', async (req, res) => {
     res.json({ error: 'Server error' });
   }
 });
+
+//add exercice to user log
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  const userId = req.params._id;
+  const { description, duration, date } = req.body;
+
+  // Validation des champs description et duration
+  if (!description || !duration) {
+    return res.status(400).json({ error: 'Description and duration are required' });
+  }
+
+  try {
+    // Rechercher l'utilisateur par _id
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Créer l'objet de l'exercice
+    const exercise = {
+      description,
+      duration: parseInt(duration),
+      date: date ? new Date(date) : new Date() // Utilise la date fournie ou la date actuelle
+    };
+
+    // Ajouter l'exercice au log de l'utilisateur
+    user.log.push(exercise);
+
+    // Sauvegarder l'utilisateur mis à jour
+    await user.save();
+
+    // Répondre avec l'objet utilisateur et l'exercice ajouté, formaté comme attendu
+    res.status(201).json({
+      username: user.username,
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date.toDateString(), // Formater la date
+      _id: user._id
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+//get user log exercice
+app.get('/api/users/:_id/logs', async(req,res)=>{
+  const userId =req.params._id
+  try {
+    const user = await User.findById(userId)
+    if(!user){
+      res.status(404).json({message:'user dont found'})
+    }
+
+    // Formater les dates des exercices dans le log
+    const formattedLog = user.log.map(exercise => ({
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date.toDateString() // Formater la date
+    }));
+
+    res.status(200).json({
+      username : user.username,
+      count: user.log.length,
+      _id: user._id,
+      log: formattedLog
+    })
+  } catch (error) {
+    res.status(500).json({error:'server error'})
+  }
+})
 
 //get all users
 app.get('/api/users', async(req,res)=>{
